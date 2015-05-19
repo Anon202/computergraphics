8a8855e7a4c32681eafae43733a62b9b97b8481d
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2015-05-19 20:51:40 +0200

    Update cartoon shader to support material properties

--- shaders/cartoonfs.glsl
+++ shaders/cartoonfs.glsl
@@ -1,5 +1,7 @@
 #version 450 core
 
+uniform vec3 mAmbient;
+
 in vec3 normalInterp;
 in vec3 vposition;
 out vec4 fColor;
@@ -11,13 +13,13 @@ void main(void) {
     vec3 vlight = normalize(light - vposition);
     float intensity = dot(vlight, normal); 
     if (intensity > 0.95)
-        fColor = vec4(0.5, 0.5, 1.0, 1.0);
+        fColor = vec4(mAmbient*4, 1.0);
     else if (intensity > 0.75)
-        fColor = vec4(0.4, 0.4, 0.8, 1.0);
+        fColor = vec4(mAmbient*2, 1.0);
     else if (intensity > 0.5)
-        fColor = vec4(0.3, 0.3, 0.6, 1.0);
+        fColor = vec4(mAmbient, 1.0);
     else if (intensity > 0.25)
-        fColor = vec4(0.2, 0.2, 0.4, 1.0);
+        fColor = vec4(mAmbient/2, 1.0);
     else
-        fColor = vec4(0.1, 0.1, 0.2, 1.0);
+        fColor = vec4(mAmbient/4, 1.0);
 }

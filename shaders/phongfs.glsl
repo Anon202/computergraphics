462fd3364a3372cf1bc2bd2fadc99e7a3d9c9590
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2015-05-19 18:44:33 +0200

    Use each model specific properties in Phong shading

--- shaders/phongfs.glsl
+++ shaders/phongfs.glsl
@@ -1,5 +1,10 @@
 #version 450 core
 
+uniform vec3 mAmbient;
+uniform vec3 mDiffuse;
+uniform vec3 mSpecular;
+uniform float mShininess;
+
 in vec3 normalInterp;
 in vec3 vposition;
 out vec4 fColor;
@@ -9,13 +14,6 @@ struct LightData {
     vec3 ambient;
     vec3 diffuse;
     vec3 specular;
-};
-
-struct MaterialData {
-    vec3 ambient;
-    vec3 diffuse;
-    vec3 specular;
-    float shininess;
 };
 
 LightData[2] lights = {
@@ -33,13 +31,6 @@ LightData[2] lights = {
     )
 };
 
-MaterialData material = MaterialData(
-    vec3(0.3, 0.0, 0.0),
-    vec3(0.5, 0.0, 0.0),
-    vec3(1.0, 1.0, 1.0),
-    5.0
-);
-
 void main(void) {
     vec3 v = normalize(-vposition);
     vec3 n = normalize(normalInterp);
@@ -47,10 +38,10 @@ void main(void) {
     for (int i = 0; i < 2; i++) {
         vec3 l = normalize(lights[i].position - vposition);
         vec3 r = reflect(-l, n);
-        vec3 ambient = material.ambient * lights[i].ambient;
-        vec3 diffuse = material.diffuse * lights[i].diffuse * max(dot(n, l), 0.0);
-        vec3 specular = material.specular * lights[i].specular * 
-                        pow(max(dot(r, v), 0.0), material.shininess);
+        vec3 ambient = mAmbient * lights[i].ambient;
+        vec3 diffuse = mDiffuse * lights[i].diffuse * max(dot(n, l), 0.0);
+        vec3 specular = mSpecular * lights[i].specular * 
+                        pow(max(dot(r, v), 0.0), mShininess);
         color += ambient + diffuse + specular;
     }
     fColor = vec4(color, 1.0);

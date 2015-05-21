b8817daed8c449beaf0b45d8060b64b2e008058a
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2015-05-21 13:34:24 +0200

    Update cartoon shader to have brighter colors

--- shaders/cartoonfs.glsl
+++ shaders/cartoonfs.glsl
@@ -13,13 +13,13 @@ void main(void) {
     vec3 vlight = normalize(light - vposition);
     float intensity = dot(vlight, normal); 
     if (intensity > 0.95)
-        fColor = vec4(mAmbient*4, 1.0);
+        fColor = vec4(mAmbient*3, 1.0);
     else if (intensity > 0.75)
-        fColor = vec4(mAmbient*2, 1.0);
+        fColor = vec4(mAmbient*1.5, 1.0);
     else if (intensity > 0.5)
         fColor = vec4(mAmbient, 1.0);
     else if (intensity > 0.25)
-        fColor = vec4(mAmbient/2, 1.0);
+        fColor = vec4(mAmbient/1.5, 1.0);
     else
-        fColor = vec4(mAmbient/4, 1.0);
+        fColor = vec4(mAmbient/3, 1.0);
 }

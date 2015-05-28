a2a4d3b9f18cd050344e3b64f1cd104b45e6adea
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2015-05-28 23:50:16 +0200

    Add const to Vector functions that don't modify this

--- algebra/Vector.cpp
+++ algebra/Vector.cpp
@@ -8,51 +8,51 @@ Vector::Vector(float x, float y, float z) : x(x), y(y), z(z) {}
 
 Vector::Vector() : x(0), y(0), z(0) {}
 
-Vector Vector::Add(Vector v) {
+Vector Vector::Add(Vector v) const {
     return Vector(this->x + v.x, this->y + v.y, this->z + v.z);
 }
 
-Vector Vector::Subtract(Vector v) {
+Vector Vector::Subtract(Vector v) const {
     return Vector(this->x - v.x, this->y - v.y, this->z - v.z);
 }
 
-Vector Vector::Cross(Vector v) {
+Vector Vector::Cross(Vector v) const {
     float newx = this->y*v.z - this->z*v.y;
     float newy = this->z*v.x - this->x*v.z;
     float newz = this->x*v.y - this->y*v.x;
     return Vector(newx, newy, newz);
 }
 
-float Vector::Dot(Vector v) {
+float Vector::Dot(Vector v) const {
     return this->x*v.x + this->y*v.y + this->z*v.z;
 }
 
-float Vector::Length() {
+float Vector::Length() const {
     return sqrt(this->x*this->x + this->y*this->y + this->z*this->z); 
 }
 
-Vector Vector::Normalized() {
+Vector Vector::Normalized() const {
     float length = this->Length();
     return Vector(this->x/length, this->y/length, this->z/length);
 }
 
-Vector Vector::ScalarMult(float c) {
+Vector Vector::ScalarMult(float c) const {
     return Vector(this->x*c, this->y*c, this->z*c);
 }
 
-void Vector::Print(const char *name) {
+void Vector::Print(const char *name) const {
     cout << name << ": " << *this << endl;
 }
 
-Vector Vector::operator-() {
+Vector Vector::operator-() const {
     return this->ScalarMult(-1);
 }
 
-Vector Vector::operator-(const Vector& rhs) {
+Vector Vector::operator-(const Vector& rhs) const {
     return this->Subtract(rhs);
 }
 
-Vector Vector::operator+(const Vector& rhs) {
+Vector Vector::operator+(const Vector& rhs) const {
     return this->Add(rhs);
 }
 
@@ -64,6 +64,10 @@ Vector& Vector::operator +=(const Vector& v) {
    this->y += v.y;
    this->z += v.z;
    return *this;
+}
+
+Vector Vector::operator*(float f) const {
+    return this->ScalarMult(f);
 }
 
 ostream& operator<<(ostream &o, const Vector& v) {
--- algebra/Vector.h
+++ algebra/Vector.h
@@ -9,23 +9,29 @@ namespace algebra {
 
 class Vector {
 public:
-    float x, y, z;
+    union {
+        struct { float x, y, z; };
+        struct { float r, g, b; };
+    };
     Vector();
     Vector(float x, float y, float z);
-    Vector Add(Vector v);
-    Vector Subtract(Vector v);
-    Vector Cross(Vector v);
-    float Dot(Vector v);
-    float Length();
-    Vector Normalized();
-    Vector ScalarMult(float c);
-    void Print(const char *name);    
-    Vector operator-();
-    Vector operator-(const Vector& rhs);
-    Vector operator+(const Vector& rhs);
+    Vector Add(Vector v) const;
+    Vector Subtract(Vector v) const;
+    Vector Cross(Vector v) const;
+    float Dot(Vector v) const;
+    float Length() const;
+    Vector Normalized() const;
+    Vector ScalarMult(float c) const;
+    void Print(const char *name) const;    
+    Vector operator-() const;
+    Vector operator-(const Vector& rhs) const;
+    Vector operator+(const Vector& rhs) const;
     Vector& operator +=(const Vector& v);
+    Vector operator*(float f) const;
     friend ostream& operator<<(ostream &o, const Vector& v);
 };
+
+typedef Vector Color;
 
 }
 
